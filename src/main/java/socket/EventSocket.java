58f0500b648ea05dfb4b383854558e904f81d573
Privatik3 <lol11999333@gmail.com>
2018-07-20 16:14:18 +0300

    COMMIT

--- src/main/java/socket/EventSocket.java
+++ src/main/java/socket/EventSocket.java
@@ -13,17 +13,33 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @ClientEndpoint
-@ServerEndpoint(value = "/task/")
+@ServerEndpoint(value = "/task")
 public class EventSocket {
 
+    private static List<String> allTokens = new ArrayList<>();
     private Session sess;
 
     @OnOpen
     public void onWebSocketConnect(Session sess) throws IOException {
-        this.sess = sess;
-        System.out.println("Socket Connected: " + sess);
+        String token = getToken(sess);
+        if (allTokens.contains(token)) {
+            sess.close();
+            System.out.println("ТАКОЙ ТОКЕН УЖЕ ОТКРЫТ!!!");
+        } else {
+            System.out.println("ТОКЕН ДОБАВЛЕН В БАЗУ: " + token);
+            allTokens.add(token);
+            this.sess = sess;
+        }
+    }
+
+    private String getToken(Session sess) {
+        Map<String, List<String>> requestParam = sess.getRequestParameterMap();
+        return requestParam.get("token").get(0);
     }
 
     @OnMessage
@@ -31,7 +47,7 @@ public class EventSocket {
         System.out.println("Received TEXT json: " + json);
 //        json = "{\"message\":\"start\",\"parameters\":[{\"name\":\"view\",\"value\":\"\"},{\"name\":\"min_id\",\"value\":\"\"},{\"name\":\"q\",\"value\":\"\"},{\"name\":\"search[city_id]\",\"value\":\"\"},{\"name\":\"search[region_id]\",\"value\":\"\"},{\"name\":\"search[district_id]\",\"value\":\"0\"},{\"name\":\"search[dist]\",\"value\":\"0\"},{\"name\":\"search[filter_float_price:from]\",\"value\":\"\"},{\"name\":\"search[filter_float_price:to]\",\"value\":\"\"},{\"name\":\"search[category_id]\",\"value\":\"\"},{\"name\":\"page\",\"value\":\"1\"}]}";
 
-        System.out.println("MESSAGE FROM: " + sess.getId());
+//        System.out.println("MESSAGE FROM: " + sess.getId());
 
         /*JSONObject obj = new JSONObject(json);
         String message = obj.getString("message");
@@ -52,7 +68,9 @@ public class EventSocket {
 
     @OnClose
     public void onWebSocketClose(CloseReason reason) {
-        System.out.println("Socket Closed: " + reason);
+        String token = getToken(sess);
+        System.out.println("ЗАКРЫВАЮ СОЕДЕНЕНИЕ, TOKEN: " + token);
+        allTokens.remove(token);
     }
 
     @OnError
