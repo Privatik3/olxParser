7c7b988823098ee25359b212fa74c5d84dbe4a44
Privatik3 <lol11999333@gmail.com>
2018-07-20 04:17:20 +0300

    COMMIT

--- src/main/java/socket/EventSocket.java
+++ src/main/java/socket/EventSocket.java
@@ -1,5 +1,9 @@
 package socket;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.websocket.ClientEndpoint;
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
@@ -8,6 +12,7 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.util.*;
 
 @ClientEndpoint
 @ServerEndpoint(value = "/task/")
@@ -18,8 +23,67 @@ public class EventSocket {
     }
 
     @OnMessage
-    public void onWebSocketText(String message) {
-        System.out.println("Received TEXT message: " + message);
+    public void onWebSocketText(String json) {
+        System.out.println("Received TEXT json: " + json);
+        JSONObject obj = new JSONObject(json);
+        String message = obj.getJSONObject("message").getString("message");
+
+        switch (message) {
+            case "start": {
+                System.out.println("Получена команда к старту парсинга, ПАРАМЕТРЫ:");
+                JSONArray params = obj.getJSONArray("parameters");
+                for (int i = 0; i < params.length(); i++) {
+                    System.out.println(jsonToMap(params.getJSONObject(i)));
+                }
+            }
+        }
+
+
+    }
+
+    public static Map<String, Object> jsonToMap(JSONObject json) throws JSONException {
+        Map<String, Object> retMap = new HashMap<String, Object>();
+
+        if(json != JSONObject.NULL) {
+            retMap = toMap(json);
+        }
+        return retMap;
+    }
+
+    public static Map<String, Object> toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    public static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
     }
 
     @OnClose
